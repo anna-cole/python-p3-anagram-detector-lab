c84d2184b72fe826af097aa053d59708ba5807b2
Anna Raptidou Cole <acrrj123@gmail.com>
2023-12-29 14:47:44 -0600

    Lab done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,22 @@
 # your code goes here!
+
+class Anagram:
+
+  def __init__(self, word):
+    self.word = word
+
+  def match(self, word_list):
+    anagrams = []
+
+    letters_in_word = [letter for letter in self.word]
+
+    for element in word_list:
+      letters_in_list = [letter for letter in element]
+      if sorted(letters_in_list) == sorted(letters_in_word):
+        anagrams.append(element)
+        #print(anagrams)
+        
+    return anagrams
+
+listen = Anagram("enlist")
+listen.match(["listen", "silent", "hippopotamus"])
